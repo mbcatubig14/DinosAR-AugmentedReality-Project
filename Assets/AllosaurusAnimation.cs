6df92d8ccb0adc9dbe97416696b063f1a4b97d32
Muhammad Catubig <mbcatubig_14@yahoo.com.ph>
2016-04-29 22:04:27 +0100

    First Augmented Reality Commit

---
+++ Assets/AllosaurusAnimation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AllosaurusAnimation : MonoBehaviour
+{
+
+	Animation anim;
+	AudioSource audio;
+
+	IEnumerator Start ()
+	{
+		anim = GetComponent<Animation> ();
+		audio = GetComponent<AudioSource> ();
+		yield return new WaitForSeconds (anim.clip.length);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
+			anim.Play ("Allosaurus_IdleBellow");
+			audio.PlayDelayed (1.5f);
+		}
+	}
+}
---
+++ Assets/DinoInfo.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DinoInfo : MonoBehaviour {
+
+	public GameObject dinoImageTarget;
+	public Text infoText;
+	bool isPressed = false;
+
+
+	void Update(){
+		if (isPressed) {
+			Allosaurus ();
+			TRex ();
+		} else {
+			infoText.enabled = false;
+		}
+	}
+
+	public void Allosaurus(){
+		if (dinoImageTarget.name == "AllosaurusImageTarget") {
+			infoText.enabled = true;
+		}
+	}
+
+	public void TRex(){
+		if (dinoImageTarget.name == "T-RexImageTarget") {
+			infoText.enabled = true;
+		}
+	}
+
+	public void ToggleChanged(bool newValue){
+		isPressed = newValue;
+	}
+
+}
